b6cc220909a4c6af7100dea895d654e21ba3dba2
akbay74 <asv1974@list.ru>
2024-09-13 11:26:36 +0400

    Add files via upload

--- classandobjects.py
+++ classandobjects.py
@@ -16,13 +16,65 @@ class House:
     def __str__(self):
         return(f"Название: {self.name}, кол-во этажей: {self.num_of_floors}")
 
+    def check_for_int(self, other):
+        return isinstance(self.num_of_floors, int) and isinstance(other.num_of_floors, int)
+
+    def __eq__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors == other.num_of_floors
+
+    def __lt__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors < other.num_of_floors
+
+    def __le__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors <= other.num_of_floors
+
+    def __gt__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors > other.num_of_floors
+
+    def __ge__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors >= other.num_of_floors
+
+    def __ne__(self, other):
+        if self.check_for_int(self):
+            return self.num_of_floors != other.num_of_floors
+
+    def __add__(self, value):
+        if self.check_for_int(self):
+            self.num_of_floors += value
+        return self
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        return self + other
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
-print(str(h1))
-print(str(h2))
+print(h1)
+print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
